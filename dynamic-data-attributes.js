2bb5d6e4d34f5dee914dc51cb471815be0468391
techboyg5 <63317676+techboyg5@users.noreply.github.com>
2020-09-27 11:18:43 -0500

    Create dynamic-data-attributes.js

---
+++ dynamic-data-attributes.js
@@ -0,0 +1,4 @@
+document.documentElement.setAttribute("data-href", window.location.href);
+document.documentElement.setAttribute("data-host", window.location.host);
+document.documentElement.setAttribute("data-referrer", document.referrer);
+document.documentElement.setAttribute("data-user-agent", navigator.userAgent);
