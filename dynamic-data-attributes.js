e5f752ff4608514e8e1216ef6853e1a0ce864595
techboyg5 <63317676+techboyg5@users.noreply.github.com>
2020-09-27 12:08:57 -0500

    Add data attribute API

--- dynamic-data-attributes.js
+++ dynamic-data-attributes.js
@@ -1,10 +1,18 @@
-document.documentElement.setAttribute("data-href", window.location.href);
-document.documentElement.setAttribute("data-protocol", window.location.protocol);
-document.documentElement.setAttribute("data-host", window.location.host);
-document.documentElement.setAttribute("data-hostname", window.location.hostname);
-document.documentElement.setAttribute("data-port", window.location.port);
-document.documentElement.setAttribute("data-pathname", window.location.pathname);
-document.documentElement.setAttribute("data-search", window.location.search);
-document.documentElement.setAttribute("data-hash", window.location.hash);
-document.documentElement.setAttribute("data-referrer", document.referrer);
-document.documentElement.setAttribute("data-user-agent", navigator.userAgent);
+addDataAttr("href", window.location.href);
+addDataAttr("protocol", window.location.protocol);
+addDataAttr("host", window.location.host);
+addDataAttr("hostname", window.location.hostname);
+addDataAttr("port", window.location.port);
+addDataAttr("pathname", window.location.pathname);
+addDataAttr("search", window.location.search);
+addDataAttr("hash", window.location.hash);
+addDataAttr("referrer", document.referrer);
+addDataAttr("user-agent", navigator.userAgent);
+
+function addDataAttr(name, value) {
+  document.documentElement.setAttribute("data-" + name, value);
+}
+
+function removeDataAttr(name) {
+  document.documentElement.removeAttribute("data-" + name);
+}
